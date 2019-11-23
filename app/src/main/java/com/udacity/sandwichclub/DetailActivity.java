61eab1ccff20505e0db3522d77b702bfb1d9af69
mitnotromney <timharrisco@gmail.com>
2019-11-23 17:10:00 -0600

    Project Finish

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,24 +3,39 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import org.json.JSONException;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+    Sandwich sandwich = null;
+
+    ImageView ingredientsIv;
+    TextView mAlsoKnow;
+    TextView mOrigin;
+    TextView mDesc;
+    TextView mIngredients;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+        ingredientsIv = findViewById(R.id.image_iv);
+        mAlsoKnow = findViewById(R.id.also_known_tv);
+        mOrigin = findViewById(R.id.origin_tv);
+        mDesc = findViewById(R.id.description_tv);
+        mIngredients = findViewById(R.id.ingredients_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -36,9 +51,14 @@ public class DetailActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+
+        try {
+            sandwich = JsonUtils.parseSandwichJson(json);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         if (sandwich == null) {
-            // Sandwich data unavailable
+
             closeOnError();
             return;
         }
@@ -57,6 +77,53 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void populateUI() {
+        String description = "";
+        if (sandwich.getAlsoKnownAs() != null && sandwich.getAlsoKnownAs().size() != 0) {
+            description = mAlsoKnow.getText().toString() + " ";
+            for (int i = 0; i < sandwich.getAlsoKnownAs().size(); i++) {
+                if (i == 0) {
+                    description = description + sandwich.getAlsoKnownAs().get(i);
+                } else {
+                    description = description + ", " + sandwich.getAlsoKnownAs().get(i);
+                }
 
+                mAlsoKnow.setVisibility(View.VISIBLE);
+                mAlsoKnow.setText(description);
+            }
+        } else {
+            mAlsoKnow.setVisibility(View.INVISIBLE);
+        }
+
+        description = mDesc.getText().toString() + " ";
+
+        if (sandwich.getDescription() != null && !sandwich.getDescription().isEmpty()) {
+            mDesc.setText(description + sandwich.getDescription());
+        } else {
+            mDesc.setVisibility(View.INVISIBLE);
+        }
+
+        description = mOrigin.getText().toString() + " ";
+
+        if (sandwich.getPlaceOfOrigin() != null && !sandwich.getPlaceOfOrigin().isEmpty()) {
+            mOrigin.setText(description + sandwich.getPlaceOfOrigin());
+        } else {
+            mOrigin.setVisibility(View.INVISIBLE);
+        }
+
+        if (sandwich.getIngredients() != null && sandwich.getIngredients().size() != 0) {
+            String ingred = mIngredients.getText().toString() + " ";
+            for (int i =0; i < sandwich.getIngredients().size(); i++) {
+                if (i == 0) {
+                    ingred = ingred + sandwich.getIngredients().get(i);
+                } else {
+                    ingred = ingred + ", " + sandwich.getIngredients().get(i);
+                }
+
+                mIngredients.setVisibility(View.VISIBLE);
+                mIngredients.setText(ingred);
+            }
+        } else {
+            mIngredients.setVisibility(View.INVISIBLE);
+        }
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,56 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
-        return null;
+    public static Sandwich parseSandwichJson(String json) throws JSONException {
+        /* Details for SW name */
+        final String SW_NAME_DETAILS = "name";
+
+        /* Name of SW */
+        final String SD_NAME = "mainName";
+
+        final String SD_ALIAS = "alsoKnownAs";
+
+        final String SD_ORIGIN = "placeOfOrigin";
+
+        final String SD_DESC = "description";
+
+        final String SD_IMAGE = "image";
+
+        final String SD_ING = "ingredients";
+
+        JSONObject sandwichJO = new JSONObject(json);
+
+        JSONObject sand_name = sandwichJO.getJSONObject(SW_NAME_DETAILS);
+
+        String mainName = sand_name.getString(SD_NAME);
+        JSONArray alsoKnownArray = sand_name.getJSONArray(SD_ALIAS);
+        List<String> alsoKnownAs = jsonArrayToList(alsoKnownArray);
+
+        String placeOfOrigin = sandwichJO.getString(SD_ORIGIN);
+        String swDesc = sandwichJO.getString(SD_DESC);
+        String swIamge = sandwichJO.getString(SD_IMAGE);
+
+        JSONArray swIngredients = sandwichJO.getJSONArray(SD_ING);
+        List<String> swIngred = jsonArrayToList(swIngredients);
+
+        return new Sandwich(mainName, alsoKnownAs, placeOfOrigin, swDesc, swIamge, swIngred);
+    }
+
+    public static List<String> jsonArrayToList(JSONArray rawArray) throws JSONException {
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < rawArray.length(); i++) {
+            list.add(rawArray.getString(i));
+        }
+        return list;
     }
 }
